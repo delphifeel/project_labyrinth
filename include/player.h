ce5bae57f7aa76b710a3f4b2962ab8c73eb5f548
delphifeel <delphifeel@gmail.com>
2021-08-14 21:40:33 +0300

    * added Player, Command interface

---
+++ include/player.h
@@ -0,0 +1,36 @@
+#ifndef __PLAYER_H__
+#define __PLAYER_H__
+
+#include "../CORE.h"
+
+/*****************************************************************************************************************************/
+
+CORE_OBJECT_INTERFACE(Player,
+	uint32 	Id;
+	char 	Name[40];
+	// TODO: Position
+)
+
+typedef enum MoveDirection
+{
+	kMoveDirection_Top,
+	kMoveDirection_Right,
+	kMoveDirection_Bottom,
+	kMoveDirection_Left,
+} MoveDirection;
+
+/*****************************************************************************************************************************/
+
+void Player_Move(Player, MoveDirection *Directions);
+
+void Player_SetId(Player, uint32 Id);
+void Player_GetId(Player, uint32 *Id);
+void Player_SetName(Player, char *Name);
+void Player_GetName(Player, char **Name);
+
+void Player_Create(Player*);
+void Player_Free(Player*);
+
+/*****************************************************************************************************************************/
+
+#endif
