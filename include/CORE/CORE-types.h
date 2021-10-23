b0d6a79ae35c4df96e8348ae72c8f2a107dafa9b
delphifeel <delphifeel@gmail.com>
2021-10-23 20:05:31 +0300

    * [CORE-types] changed

--- include/CORE/CORE-types.h
+++ include/CORE/CORE-types.h
@@ -6,16 +6,16 @@
 #define TRUE                                                    (1)
 #define FALSE                                                   (0)
 
-typedef unsigned char                                           uint8;                              /* unsigned */
-typedef unsigned short int                                      uint16;
-typedef unsigned long int                                       uint32;
-typedef unsigned long long                                      uint64;
-typedef signed char                                             int8;                               /* signed */
-typedef signed short int                                        int16;
-typedef signed long int                                         int32;
-typedef signed long long                                        int64;
-typedef float                                                   float32;                            /* single precision floating point */
-typedef double                                                  float64;                            /* double precision floating point */
-typedef signed long int 										CORE_Bool;
+typedef uint8_t                                       uint8;                              /* unsigned */
+typedef uint16_t                                      uint16;
+typedef uint32_t                                      uint32;
+typedef uint64_t                                      uint64;
+typedef int8_t                                        int8;                               /* signed */
+typedef int16_t                                       int16;
+typedef int32_t                                       int32;
+typedef int64_t                                       int64;
+typedef float                                         float32;                            /* single precision floating point */
+typedef double                                        float64;                            /* double precision floating point */
+typedef int32_t 									  CORE_Bool;
 
 #endif
